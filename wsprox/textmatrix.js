628c7e1e65a242c14ed81103a3d11368fe6ed20e
Nava Whiteford <new@sgenomics.org>
2015-02-06 06:23:26 -0500

    refactored display into local cache (textmatrix_displaydata) prior to adding colorisation

--- wsprox/textmatrix.js
+++ wsprox/textmatrix.js
@@ -1,34 +1,69 @@
-function add_textmatrix(x,y,loc) {
+var textmatrix_displaydata = []; // array arranged by y x
+
+function add_textmatrix(x,y) {
 
   for(var cy=0;cy<y;cy++) {
 
     cline = "<div style=\"display: inline\" id=\"l" + cy + "\">"
+
+    textmatrix_displaydata.push([]);
     for(var cx=0;cx<x;cx++) {
       cline += "A";
+      textmatrix_displaydata[cy].push({"char":"A", "fg":0xFFFFFF, "bg":0x000000});
     }
     cline += "</div><br>";
     document.getElementById('tml').innerHTML += cline;
   }
 }
 
-function stridx(str,index, character) {
-    return str.substr(0, index) + character + str.substr(index+character.length);
+//function stridx(str,index, character) {
+//    return str.substr(0, index) + character + str.substr(index+character.length);
+//}
+
+function textmatrix_setpos(x,y,c,fg,bg) {
+
+  textmatrix_displaydata[y][x].char = c;
+  textmatrix_displaydata[y][x].fg   = fg;
+  textmatrix_displaydata[y][x].bg   = bg;
+
+  textmatrix_redraw_line(y);
 }
 
-function textmatrix_setpos(x,y,c) {
+function textmatrix_redraw_line(y) {
 
-  var line = document.getElementById('l' + y).innerHTML;
+  var newline;
 
-  var newline = stridx(line,x,c);
+//  for(var cx=0;cx<textmatrix_displaydata[y].length;cx++) {
+//    newline += textmatrix_displaydata[y][cx].char;
+//  }
+  newline = textmatrix_getline(y);
+  
+
+  //<p style="color: #000000; background-color: #ffffff; display: inline">Text</p><p style="color: #ffffff; background-color: #000000; display: inline">Text</p>
+  //var line = document.getElementById('l' + y).innerHTML;
+
+  //var newline = stridx(line,x,c);
 
   document.getElementById('l' + y).innerHTML = newline;
 }
 
 function textmatrix_setline(y,newline) {
-  document.getElementById('l' + y).innerHTML = newline;
+//  console.debug("newline: " + newline);
+  for(var cx=0;cx<newline.length;cx++) {
+    textmatrix_displaydata[y][cx].char = newline.substr(cx,1);
+  }
+  textmatrix_redraw_line(y);
+
+//  document.getElementById('l' + y).innerHTML = newline;
 }
 
 function textmatrix_getline(y) {
-  var line = document.getElementById('l' + y).innerHTML;
-  return line;
+  var data ="";
+  for(var cx=0;cx<textmatrix_displaydata[y].length;cx++) {
+    data = data + textmatrix_displaydata[y][cx].char;
+  }
+
+  return data;
+//  var line = document.getElementById('l' + y).innerHTML;
+//  return line;
 }
